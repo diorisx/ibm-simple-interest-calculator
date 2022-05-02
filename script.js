5eeeb0a8c72e2d7f90b74c553e2ff7ce8b57a332
drenvill <dioris164@gmail.com>
2022-05-02 02:04:47 -0400

    final work

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
-function compute()
-{
-    p = document.getElementById("principal").value;
-    
-}
+
+// var interst_rate = document.getElementById("int_rate");
+
+    function compute()
+    {
+        p = document.getElementById("principal").value;
+        rate = document.getElementById("rate").value;
+        year = document.getElementById("years").value;
+        result = document.getElementById("result");
+
+        var P = p * (1+(rate/100) * year);
+        var A = P;
+        var RESULT = A -p;
+        result.innerHTML = `is you deposit ${p} <br>at 
+                an interest of ${rate}. <br> 
+                 You will receive an amount of ${RESULT}`;
+
+                
+    }
+
+
+    function noYear(value){
+            document.getElementById("int_rate").innerHTML = value+"%";
         
+    }
+
+
+
